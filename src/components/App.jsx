1e63323f1c9748d6085a8d9c671b00da15b47c79
Taiga KIYOKAWA <taiga.k127@gmail.com>
2019-07-18 17:39:46 +0900

    add text-field and submit-button in Form and import it to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Header';
+import Form from './Form';
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
         <Header/>
+        <Form/>
       </div>
     );
   }
---
+++ src/components/Form.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Form = () => (
+  <form action="">
+    <div>
+      <input type="text" name="title"/>
+      <input type="submit" value="CREATE"/>
+    </div>
+  </form>
+)
+
+export default Form;
