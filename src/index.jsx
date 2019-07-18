0428379513b55b654284f21e8dc33d64f4daf490
Taiga KIYOKAWA <taiga.k127@gmail.com>
2019-07-19 01:52:25 +0900

    move style.css into assets

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
-import './style.css';
+import './assets/style.css';
 
 render(<App />, document.getElementById('root'));
