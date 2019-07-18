e943d4f62057fbf186ea1ca2974f4b2ec9521a12
Taiga KIYOKAWA <taiga.k127@gmail.com>
2019-07-18 17:48:18 +0900

    define createTodo func in App and give it to Form as props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,11 +10,26 @@ export default class App extends React.Component {
     };
   }
 
+  // Use in <Form/>: Create a new todo{title: string}
+  createTodo = (e) => {
+    // Prevent redirect
+    e.preventDefault();
+    const newTodo = e.target.title.value;
+    console.log(`create: ${newTodo}`);
+    const todoList = [...this.state.todoList, { title: newTodo }];
+    // Update state
+    this.setState({ todoList: todoList });
+    // Reset <Form/>'s input value
+    e.target.title.value = "";
+  }
+
   render() {
+    console.log("--- updated ---");
+    console.log(this.state.todoList);
     return (
       <div>
         <Header/>
-        <Form/>
+        <Form createTodo={ this.createTodo }/>
       </div>
     );
   }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const Form = () => (
-  <form action="">
+const Form = (props) => (
+  <form onSubmit={ props.createTodo }>
     <div>
       <input type="text" name="title"/>
       <input type="submit" value="CREATE"/>
