c28816262b66ded39583e0fc65bf0c849b37bca6
Taiga KIYOKAWA <taiga.k127@gmail.com>
2019-07-18 19:53:31 +0900

    update styles(change color and import some fonts from Google)

--- src/components/AllDone.jsx
+++ src/components/AllDone.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const AllDone = (props) => (
-  <div>
+  <div className="allDone">
     <input 
       type="checkbox"
       checked={ props.isAllDone }
