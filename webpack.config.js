f61d83548f8d81cc20d34f48b05980905a8b5356
Taiga KIYOKAWA <taiga.k127@gmail.com>
2019-07-18 16:52:07 +0900

    add entry, output, module, resolve, pulgins to webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+import path from 'path'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+
+const src = path.resolve(__dirname, 'src')
+const dist = path.resolve(__dirname, 'dist')
+
+module.exports = {
+  entry: src + '/index.jsx',
+
+  output: {
+    path: dist,
+    filename: 'bundle.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: src + '/index.html',
+      filename: 'index.html'
+    })
+  ]
+}
